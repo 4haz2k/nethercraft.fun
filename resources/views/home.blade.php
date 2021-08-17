1aa352c7afd2472e86608062f1836d597a230255
AS2005071645\Пользователь <zytia@mail.ru>
2021-08-17 21:45:22 +0300

    70% done main page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -21,11 +21,25 @@
 <div class="g-navigation">
     <div class="header-nav">
         <div class="container-fluid">
-            <div class="row p-2">
+            <nav role="navigation" class="mobile-menu d-block d-sm-none">
+                <div id="menuToggle">
+                    <input type="checkbox" />
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <ul id="menu">
+                        <li><a href="#">Главная</a></li>
+                        <li><a href="#">О донате</a></li>
+                        <li><a href="#">Магазин</a></li>
+                        <li><a href="#">Правила</a></li>
+                    </ul>
+                </div>
+            </nav>
+            <div class="row p-2 top-nav">
                 <div class="col-sm-3 text-nav logo">
                     <a href="">NetherCraft Project</a>
                 </div>
-                <div class="col-sm-6 d-none d-sm-block" style="padding-top: 5px">
+                <div class="col-sm-6 d-none d-sm-block text-center" style="padding-top: 5px">
                     <div class="row text-nav">
                         <div class="col-3 w-50"><a href="" class="a-none social">Главная</a></div>
                         <div class="col-3 w-50"><a href="" class="a-none social">О донате</a></div>
@@ -33,7 +47,7 @@
                         <div class="col-3 w-50"><a href="" class="a-none social">Правила</a></div>
                     </div>
                 </div>
-                <div class="col-sm-3 text-center social-icons" style="padding-top: 5px">
+                <div class="col-sm-3 text-center social-icons" style="padding-top: 5px; max-width: 225px; margin: 0 auto;">
                     <div class="row">
                         <div class="col-3"><a href="" class="a-none social"><svg style="height: 24px; width: 30px;" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="youtube" class="svg-inline--fa fa-youtube fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M549.655 124.083c-6.281-23.65-24.787-42.276-48.284-48.597C458.781 64 288 64 288 64S117.22 64 74.629 75.486c-23.497 6.322-42.003 24.947-48.284 48.597-11.412 42.867-11.412 132.305-11.412 132.305s0 89.438 11.412 132.305c6.281 23.65 24.787 41.5 48.284 47.821C117.22 448 288 448 288 448s170.78 0 213.371-11.486c23.497-6.321 42.003-24.171 48.284-47.821 11.412-42.867 11.412-132.305 11.412-132.305s0-89.438-11.412-132.305zm-317.51 213.508V175.185l142.739 81.205-142.739 81.201z"></path></svg></a></div>
                         <div class="col-3"><a href="" class="a-none social"><svg style="height: 24px; width: 30px;" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="discord" class="svg-inline--fa fa-discord fa-w-20" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path fill="currentColor" d="M524.531,69.836a1.5,1.5,0,0,0-.764-.7A485.065,485.065,0,0,0,404.081,32.03a1.816,1.816,0,0,0-1.923.91,337.461,337.461,0,0,0-14.9,30.6,447.848,447.848,0,0,0-134.426,0,309.541,309.541,0,0,0-15.135-30.6,1.89,1.89,0,0,0-1.924-.91A483.689,483.689,0,0,0,116.085,69.137a1.712,1.712,0,0,0-.788.676C39.068,183.651,18.186,294.69,28.43,404.354a2.016,2.016,0,0,0,.765,1.375A487.666,487.666,0,0,0,176.02,479.918a1.9,1.9,0,0,0,2.063-.676A348.2,348.2,0,0,0,208.12,430.4a1.86,1.86,0,0,0-1.019-2.588,321.173,321.173,0,0,1-45.868-21.853,1.885,1.885,0,0,1-.185-3.126c3.082-2.309,6.166-4.711,9.109-7.137a1.819,1.819,0,0,1,1.9-.256c96.229,43.917,200.41,43.917,295.5,0a1.812,1.812,0,0,1,1.924.233c2.944,2.426,6.027,4.851,9.132,7.16a1.884,1.884,0,0,1-.162,3.126,301.407,301.407,0,0,1-45.89,21.83,1.875,1.875,0,0,0-1,2.611,391.055,391.055,0,0,0,30.014,48.815,1.864,1.864,0,0,0,2.063.7A486.048,486.048,0,0,0,610.7,405.729a1.882,1.882,0,0,0,.765-1.352C623.729,277.594,590.933,167.465,524.531,69.836ZM222.491,337.58c-28.972,0-52.844-26.587-52.844-59.239S193.056,219.1,222.491,219.1c29.665,0,53.306,26.82,52.843,59.239C275.334,310.993,251.924,337.58,222.491,337.58Zm195.38,0c-28.971,0-52.843-26.587-52.843-59.239S388.437,219.1,417.871,219.1c29.667,0,53.307,26.82,52.844,59.239C470.715,310.993,447.538,337.58,417.871,337.58Z"></path></svg></a></div>
@@ -48,7 +62,7 @@
         <div class="swiper-container" data-slideshow-autoplay="true" data-slideshow-loop="false" data-slideshow-timeout="8000" data-slideshow-speed="800">
             <div class="swiper-wrapper">
                 <!-- Slides -->
-                <div class="swiper-slide" style="background-color: green">
+                <div class="swiper-slide" style="background-image: url('{{ asset('img/pic.png') }}'); background-position: center top">
                     <div class="slide">
                         <h3 class="top-title">
                             <span>NetherCraft Project</span>
@@ -58,7 +72,7 @@
                         </h1>
                     </div>
                 </div>
-                <div class="swiper-slide" style="background-color: #ff3440">
+                <div class="swiper-slide" style="background-image: url('{{ asset('img/pic2.png') }}'); background-position: center 5vh;">
                     <div class="slide">
                         <h3 class="top-title">
                             <span>О проекте</span>
